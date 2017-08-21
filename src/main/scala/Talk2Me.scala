24cfd6d7deeb03960caf816e397c0263824372a4
prayagupd <prayag.upd@gmail.com>
2017-08-20 22:11:06 -0700

    fix dynamic server response

--- src/main/scala/Talk2Me.scala
+++ src/main/scala/Talk2Me.scala
@@ -1,6 +1,7 @@
 
 import java.net._
 import java.io._
+import java.util.Date
 
 import com.typesafe.config.ConfigFactory
 
@@ -10,17 +11,22 @@ class Talk2Me {
 
   val config = ConfigFactory.load("application.conf")
 
-  val data = Source.fromFile(config.getString("share.file1")).getLines()
-    .foldLeft(new StringBuilder)((a, b) => a append b append "\n").toString()
-
   def start(): Unit = {
     val server = new ServerSocket(9999)
-    println("[INFO] starting a server.")
+
+    import java.net._
+    val localhost = InetAddress.getLocalHost
+
+    println("[INFO] starting a server at " + localhost.getHostAddress  + ":9999")
     while (true) {
       val clientConnection = server.accept()
-      println(s"[INFO] connection established from ${clientConnection.getInetAddress}:${clientConnection.getPort}")
+      println(s"[INFO] connection established from ${clientConnection.getInetAddress}:${clientConnection.getPort} " +
+        s"at ${new Date()}")
       val request = new BufferedSource(clientConnection.getInputStream).getLines()
       val response = new PrintWriter(clientConnection.getOutputStream)
+
+      val data = Source.fromFile(config.getString("share.file1")).getLines()
+         .foldLeft(new StringBuilder)((a, b) => a append b append "\n").toString()
 
       response.println("HTTP/1.1 200\r\n")
       response.println("Content-Type text/html\r\n")
